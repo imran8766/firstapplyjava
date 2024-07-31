034aa0213f5d138323ca25c6423a74ec80a5824b
MD IMRAN SHEIKH <166485094+imran8766@users.noreply.github.com>
2024-07-31 13:46:09 +0600

    tfyf

---
+++ inchtofeet.js
@@ -0,0 +1,17 @@
+var inch = 192;
+var ans = inch / 12;
+console.log(ans);
+
+
+
+
+function intro(num){
+var ngg = num / 12;
+return ngg;
+
+}
+var total = intro(24);
+console.log(total);
+var tota = intro(1024);
+console.log(tota);
+
